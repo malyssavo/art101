9bbbfb81aeacc60d77c9c24017ba9a8af1eb4435
malyssavo <mvo3@ucsc.edu>
2022-06-07 05:35:52 -0700

    lab5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+*Author: Malyssa <mvo3@ucsc.edu>
+*Created: 21 April
+*License: Public Domain
+*/
+
+//Define Variables
+make= "Toyota";
+model= "Camry";
+color= "Midnight Black Metallic";
+year= 2022;
+
+//calculate
+age=2022-year;
+
+//Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
